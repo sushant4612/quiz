3f1fc561a24af7508baeae138959b8e76e8f07ca
sushant4612 <sushantsp14@gmail.com>
2023-08-23 19:25:16 +0530

    Quiz project Done

---
+++ question.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        num: 1,
+        question: "What does HTML stand for?",
+        answer: "A. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Text Markup Language",
+            "B. Hyperlinks and Text Markup Language",
+            "C. Home Tool Markup Language",
+            "D. High Tech Markup Language"
+        ]
+    },
+    {
+        num: 2,
+        question: "Which HTML tag is used to create a hyperlink?",
+        answer: "B. <a>",
+        options: [
+            "A. &lt;link&gt;",
+            "B. &lt;a&gt;",
+            "C. &lt;href&gt;",
+            "D. &lt;hyperlink&gt;"
+        ]
+    },
+    {
+        num: 3,
+        question: "What is the purpose of CSS?",
+        answer: "C. Styling and formatting",
+        options: [
+            "A. Adding interactivity",
+            "B. Defining webpage structure",
+            "C. Styling and formatting",
+            "D. Handling server-side logic"
+        ]
+    },
+    {
+        num: 4,
+        question: "What does CSS stand for?",
+        answer: "B. Cascading Style Sheets",
+        options: [
+            "A. Creative Style Sheets",
+            "B. Cascading Style Sheets",
+            "C. Computer Style Sheets",
+            "D. Colorful Style Sheets"
+        ]
+    },
+    {
+        num: 5,
+        question: "Which property is used to change the font size in CSS?",
+        answer: "C. font-size",
+        options: [
+            "A. font-weight",
+            "B. text-size",
+            "C. font-size",
+            "D. size"
+        ]
+    },
+    {
+        num: 6,
+        question: "What is the purpose of CSS?",
+        answer: "C. Styling and formatting",
+        options: [
+            "A. Adding interactivity",
+            "B. Defining webpage structure",
+            "C. Styling and formatting",
+            "D. Handling server-side logic"
+        ]
+    },
+    {
+        num: 7,
+        question: "Which CSS property changes the text color?",
+        answer: "C. color",
+        options: [
+            "A. text-color",
+            "B. font-color",
+            "C. color",
+            "D. text-style"
+        ]
+    },
+    {
+        num: 8,
+        question: "Which programming language is used for adding interactivity to web pages?",
+        answer: "C. JavaScript",
+        options: [
+            "A. HTML",
+            "B. CSS",
+            "C. JavaScript",
+            "D. Python"
+        ]
+    },
+    {
+        num: 9,
+        question: "Which HTML element is used for creating a list with bullet points?",
+        answer: "A. <ul>",
+        options: [
+            "A. &lt;ul&gt;",
+            "B. &lt;ol&gt;",
+            "C. &lt;dl&gt;",
+            "D. &lt;li&gt;",
+        ]
+    },
+    {
+        num: 10,
+        question: "Which tag is used to include an external JavaScript file in an HTML document?",
+        answer: 'A. <script src="filename.js"></script>',
+        options: [
+            'A. &lt;script src="filename.js"&gt;&lt;/script&gt;',
+            "B. &lt;js&gt;filename.js&lt;/js&gt;",
+            'C. &lt;javascript src="filename.js"&gt;&lt;/javascript&gt;',
+            "D. &lt;external&gt;filename.js&lt;/external&gt;"
+        ]
+    }
+]
